b4fd39a48e0e1988ccbd8727f7d28968203e8c12
Milton Munoz <livemilton@gmail.com>
2025-10-04 21:02:52 -0500

    Angular advanced :sparkles: Using Custom pipes to perform Custom Transformations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { DatePipe, DecimalPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { TemperaturaPipe } from './temperature.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [DatePipe, DecimalPipe]
+  imports: [DatePipe, DecimalPipe, TemperaturaPipe]
 })
 export class AppComponent {
   currentDate = new Date();
